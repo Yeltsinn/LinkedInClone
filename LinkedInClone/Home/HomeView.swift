a3ff2b508c8d56dfee87648f5fe42597232a5ad2
Yeltsin Gama <yeltsin.lobato@gmail.com>
2025-05-01 09:13:41 -0300

    Creating HomeView

---
+++ LinkedInClone/Home/HomeView.swift
@@ -0,0 +1,25 @@
+//
+//  HomeView.swift
+//  LinkedInClone
+//
+//  Created by Yeltsin Suares Lobato Gama on 01/05/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(0...10, id: \.self) {_ in
+                    PostView()
+                }
+            }
+        }
+        .background(Color.black)
+    }
+}
+
+#Preview {
+    HomeView()
+}
