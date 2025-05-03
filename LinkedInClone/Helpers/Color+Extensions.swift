4f1116f46efe96a1cd94771a09b092dd8e47205b
Yeltsin Gama <yeltsin.lobato@gmail.com>
2025-05-03 08:34:01 -0300

    Including custom colors to be used in the project.

---
+++ LinkedInClone/Helpers/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  LinkedInClone
+//
+//  Created by Yeltsin Suares Lobato Gama on 03/05/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let customBlue = Color(red: 59/255, green: 89/255, blue: 152/255)
+    static let customGreen = Color(red: 25/255, green: 215/255, blue: 100/255)
+    static let customRed = Color(red: 227/255, green: 28/255, blue: 95/255)
+}
