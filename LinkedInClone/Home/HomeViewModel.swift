f18d9b991f4a2883a606c601fd289cbdf779ddc9
Yeltsin Gama <yeltsin.lobato@gmail.com>
2025-05-16 08:52:50 -0300

    Adjusting HomeViewModel to be more concise.

--- LinkedInClone/Home/HomeViewModel.swift
+++ LinkedInClone/Home/HomeViewModel.swift
@@ -6,31 +6,16 @@
 //
 
 import Foundation
-import Combine
 
 class HomeViewModel: ObservableObject {
     
     @Published var posts: [Post] = []
     
-    private var cancellables = Set<AnyCancellable>()
-    
     func fetchPostFeed() {
         NetworkRequestor.getFeedItems()
             .map(\.feed)
+            .replaceError(with: [])
             .receive(on: DispatchQueue.main)
-            .sink(
-                  receiveCompletion: { completion in
-                      switch completion {
-                      case .finished:
-                          print("Finalizado com sucesso.")
-                      case .failure(let error):
-                          print("Erro ao buscar ou decodificar: \(error)")
-                      }
-                  },
-                  receiveValue: { postFeed in
-                      self.posts = postFeed
-                  }
-              )
-            .store(in: &cancellables)
+            .assign(to: &$posts)
     }
 }
